5188defdeb8a946c2949a4ddec2cbe5f587e7a1e
jacob-balslev <jacobbalslev@gmail.com>
2024-03-25 17:02:04 +0100

    2nd commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('nav a');
+    
+
+    function resume(){
+        //do something
+
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+    
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+                });
+            };
+    
+            let sectionListArray = document.querySelectorAll('#cv')
+            let headerScan = document.querySelectorAll('.hero');
+            const viewHeight = document.documentElement.clientHeight
+            
+            sectionListArray.forEach(function(v) {
+                let rect = v.getBoundingClientRect();
+                let y = rect.y;
+                let bottom = rect.bottom;
+                let height = rect.height;
+                if (y > viewHeight|| bottom+height < viewHeight ) {
+                    v.classList.remove('active')
+                } else {
+                    v.classList.add('active')
+                }
+            })
+            headerScan.forEach(function(v) {
+                let rect = v.getBoundingClientRect();
+                let y = rect.y;
+                let bottom = rect.bottom;
+                let height = rect.height;
+                if (y > viewHeight|| bottom+height < viewHeight ) {
+                    v.classList.remove('active')
+                } else {
+                    v.classList.add('active')
+                }
+            })
+            
+        });
+        }
+
+       
+
+            window.addEventListener('scroll', resume);
+    
+});
+
+
+
